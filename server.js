8988daab35d86240195817832f2532cf01abc853
ba3walos99 <38079309+ba3walos99@users.noreply.github.com>
2021-01-22 16:25:56 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('An alligator approaches!');
+});
+app.listen(80, () => console.log('Gator app listening on port 80!'));
